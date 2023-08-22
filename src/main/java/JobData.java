5ce75791e4d9c01cd272be666db5f8ad35641adf
LunaLiu4 <luna.liu829@gmail.com>
2023-08-22 17:55:18 -0500

    first commit

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -74,8 +71,8 @@ public class JobData {
         for (HashMap<String, String> row : allJobs) {
 
             String aValue = row.get(column);
-
-            if (aValue.contains(value)) {
+//            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())){
                 jobs.add(row);
             }
         }
@@ -95,7 +92,21 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+//        return null;
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        for (HashMap<String,String> row: allJobs){
+            boolean searched = false;
+            for (Map.Entry<String, String> job: row.entrySet()){
+                if (job.getValue().toLowerCase().contains(value.toLowerCase())){
+                    searched = true;
+                }
+                if (searched){
+                    jobs.add(row);
+                    searched = false;
+                }
+            }
+        }
+        return jobs;
     }
 
     /**
